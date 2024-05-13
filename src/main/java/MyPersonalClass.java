0379d9bbc005920518597ed28b856ec971e1c4d4
Gulina Sulumbekkyzy <135994322+GulinaS@users.noreply.github.com>
2024-05-12 23:32:07 -0500

    Aded more code

--- src/main/java/MyPersonalClass.java
+++ src/main/java/MyPersonalClass.java
@@ -3,5 +3,9 @@ public class MyPersonalClass {
 
         System.out.println("This is just a test for Git practice");
         System.out.println("second commit");
+
+        int num = 10;
+        System.out.println(num + "adding more line");
+
     }
 }
